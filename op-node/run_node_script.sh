c3e3b14840e288d590991cfe9a135cce461e757c
DCota <32775237+DaigaroCota@users.noreply.github.com>
2023-05-13 17:07:52 +0100

    feat: add script for running consensus

---
+++ op-node/run_node_script.sh
@@ -0,0 +1,17 @@
+source .env
+
+./bin/op-node \
+  --l1.trustrpc \
+	--l2=http://localhost:8551 \
+	--l2.jwt-secret=./jwt.txt \
+	--sequencer.enabled \
+	--sequencer.l1-confs=3 \
+	--verifier.l1-confs=3 \
+	--rollup.config=./rollup.json \
+	--rpc.addr=0.0.0.0 \
+	--rpc.port=8547 \
+	--p2p.disable \
+	--rpc.enable-admin \
+	--p2p.sequencer.key=$SEQ_KEY \
+	--l1=$L1_RPC \
+	--l1.rpckind=$RPC_KIND
